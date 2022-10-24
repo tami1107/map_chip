2be89409ed0cad83f7ced48050cc6033ef1be99d
tami <fko2247041@stu.o-hara.ac.jp>
2022-10-24 11:16:11 +0900

    設定を追加中

--- Scene/SceneTest.h
+++ Scene/SceneTest.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "SceneBase.h"
+#include "Map.h"
 
 class SceneTest : public SceneBase
 {
@@ -17,5 +18,5 @@ public:
 	virtual void draw() override;
 
 private:
-	
+	Map m_map;
 };
